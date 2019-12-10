21f99972ce8d88cab1273aeb0898e79bc8e187fe
paulinamonstaviciute <paulinamonstaviciute@gmail.com>
2019-12-10 20:04:50 +0200

    parasem antras_projektas

--- antras_projektas/antras_projektas/Program.cs
+++ antras_projektas/antras_projektas/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace antras_projektas
 {
+    class Rectangle
+    {
+        //member variables
+        private double lenght;
+        private double width;
+
+        public void Acceptdetails()
+        {
+            lenght = 4.5;
+            width = 3.5;
+        }
+        public double GetArea() 
+        {
+            return lenght * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", lenght);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Rectangle r = new Rectangle();
+            r.Acceptdetails();
+            r.Display();
+            Console.ReadLine();
         }
     }
 }
