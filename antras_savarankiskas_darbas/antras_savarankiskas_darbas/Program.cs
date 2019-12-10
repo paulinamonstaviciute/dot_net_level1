cb9b1dc08aefeced3907999082ea5c6a8f093a97
paulinamonstaviciute <paulinamonstaviciute@gmail.com>
2019-12-10 21:31:40 +0200

    parasem antras_savarankiskas_darbas

--- antras_savarankiskas_darbas/antras_savarankiskas_darbas/Program.cs
+++ antras_savarankiskas_darbas/antras_savarankiskas_darbas/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace antras_savarankiskas_darbas
 {
+    class Calculator
+    {
+        private double pirmas;
+        private double antras;
+        public void PriimtiSkaicius()
+        {
+            pirmas = 17;
+            antras = 23;
+        }
+        public double Suma()
+        {
+            return pirmas + antras;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Pirmas skaicius: {0}", pirmas);
+            Console.WriteLine("Antras skaicius: {0}", antras);
+            Console.WriteLine("suma: {0}", Suma());
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Calculator naujas_kintamasis = new Calculator();
+            naujas_kintamasis.PriimtiSkaicius();
+            naujas_kintamasis.Display();
+            Console.ReadLine();
         }
     }
 }
