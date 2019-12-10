136900e22d68487296858395d0315f4ec05b8f25
paulinamonstaviciute <paulinamonstaviciute@gmail.com>
2019-12-10 21:40:07 +0200

    parasem trecias_savarankiskas_darbas

--- trecias_savarankiskas_darbas/trecias_savarankiskas_darbas/Program.cs
+++ trecias_savarankiskas_darbas/trecias_savarankiskas_darbas/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace trecias_savarankiskas_darbas
 {
+    class Calculator
+    {
+        private double pirmas;
+        private double antras;
+        public void PriimtiSkaicius()
+        {
+            pirmas = 24;
+            antras = 5;
+        }
+        public double Dalyba()
+        {
+            return pirmas / antras;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Pirmas skaicius: {0}", pirmas);
+            Console.WriteLine("Antras skaicius: {0}", antras);
+            Console.WriteLine("Dalyba: {0}", Dalyba());
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Calculator naujas_kintamasis = new Calculator();
+            naujas_kintamasis.PriimtiSkaicius();
+            naujas_kintamasis.Display();
+            Console.ReadLine();
         }
     }
 }
